765a156602e862be9aa3651f311c95402c7f960d
boschxie <yongcongz@hotmail.com>
2024-11-18 12:31:40 +0800

    update html files and database.json and script.js

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ scrollLink.addEventListener('mouseover', () => {
   const targetElement = document.getElementById('top');
   targetElement.scrollIntoView({ behavior: 'smooth' });
 });
+
+//function for fetching data from JSON
+fetch('database.json')
+  .then(response => response.json())
+  .then(data => {
+    const filteredData = data.filter(item => item.id === 1); // Assuming ID 1 has the updated date
+
+    if (filteredData.length > 0) {
+      const updatedDate = filteredData[0].mainT;
+      const dateElement = document.getElementById('updatedDate');
+      dateElement.textContent = updatedDate;
+    } else {
+      console.error('No matching data found');
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
